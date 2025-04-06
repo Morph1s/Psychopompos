853b060c935d3fbaacf529bcb280426b858d5b4a
Morph1s <lottjedesmond@gmail.com>
2025-04-06 13:46:27 +0200

    feat: add resources for enemy and player stats closes #3

---
+++ scripts/custom_resources/player_stats.gd
@@ -0,0 +1,12 @@
+extends EntityStats
+
+@export var maximum_energy: int = 3:
+	set(value):
+		maximum_energy = value
+		# call stats changed eventbus
+@export var maximum_energy_deficit: int = 3:
+	set(value):
+		maximum_energy_deficit = value
+		# call stats changed eventbus
+
+var current_energy: int = maximum_energy
