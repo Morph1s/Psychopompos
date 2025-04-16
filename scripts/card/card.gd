bf87f285b634b7a55928dc2f851dc3ed1e7d9a63
Newton <ik066@hdm-stuttgart.de>
2025-04-16 15:38:43 +0200

    refactor: load Resource in owne initialze-methode

--- scripts/card/card.gd
+++ scripts/card/card.gd
@@ -2,15 +2,17 @@ extends Area2D
 
 signal card_play_finished
 
-var res = load("res://resources/cards/defend.tres")  # für test-Zwecke mit defend gefüllt
+var resource: Resource
 var card_type: CardType
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 
 func _ready():
 	print(rng.randi_range(0, 0))
-	card_type = res
+	card_type = resource
 	$CardImage.texture = card_type.texture 
-	
+
+func initilaize(resource) -> void:
+	resource = resource
 
 ## function to be called on playing the card
 ## 
