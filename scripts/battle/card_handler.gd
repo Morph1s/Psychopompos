e3ebafea3b6e603fbfcaf7d6932b86b014033582
Newton <ik066@hdm-stuttgart.de>
2025-04-21 19:13:16 +0200

    feat: Battel-UI correct display of sprites still in progress

--- scripts/battle/card_handler.gd
+++ scripts/battle/card_handler.gd
@@ -1,6 +1,10 @@
 class_name CardHandler
 extends Node2D
 
+signal display_draw_pile
+signal display_discard_pile
+signal end_turn
+
 var draw_pile: Array[CardType]
 var discard_pile: Array[CardType] = []
 
@@ -19,3 +23,16 @@ const STARTING_DECK: Array[CardType] = [
 
 func initialize() -> void:
 	draw_pile.append_array(STARTING_DECK)
+
+
+
+func _on_draw_pile_button_up() -> void:
+	display_draw_pile.emit()
+
+
+func _on_discard_pile_button_up() -> void:
+	display_discard_pile.emit()
+
+
+func _on_end_turn_button_button_up() -> void:
+	end_turn.emit()
