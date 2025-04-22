3954253df98a7ebd896750cc89f42c3d492ee3bc
Morph1s <lottjedesmond@gmail.com>
2025-04-22 12:36:00 +0200

    feat: creating resource instances added create instance function to entity and card type resource that needs to be called every time you use a new instance of the card or enemy closes #55

--- scripts/custom_resources/card_type.gd
+++ scripts/custom_resources/card_type.gd
@@ -4,3 +4,7 @@ extends Resource
 @export var texture : Texture
 @export var on_play_action : Array[Action]
 @export var on_graveyard_action : Array[Action]
+
+func create_instance() -> CardType:
+	var new_entity = self.duplicate()
+	return new_entity
