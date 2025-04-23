b734182352bae87dbe06d53f8c3e266825db8bb8
Rafail <90346190+rraaff46@users.noreply.github.com>
2025-04-23 12:52:56 +0200

    feat: Entity hud added nodes and functions for entity hud and a gradient as placeholder health bar

---
+++ scripts/ui/entity_hud.gd
@@ -0,0 +1,35 @@
+extends Control
+class_name EntityHud
+
+var current_hp: int = 0
+var max_hp: int = 100
+var block_value: int = 0
+
+@onready var hp_bar: TextureProgressBar = $HpBar
+@onready var block_label: Label = $BlockLabel
+@onready var hp_label: Label = $HpLabel
+
+
+func set_current_hp(value: int) -> void:
+	current_hp = value
+	_update_display()
+	
+func set_max_hp(value: int) -> void:
+	max_hp = value	
+	hp_bar.max_value = max_hp
+	_update_display()
+
+func set_block(value: int) -> void:
+	block_value = value
+	_update_display()
+
+func _update_display() -> void:
+	hp_bar.value = current_hp
+	hp_label.text = "HP: %d / %d" % [current_hp, max_hp]
+	block_label.text = "Block: %d" % block_value
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	set_current_hp(50)
+	set_max_hp(200)
+	set_block(20)
