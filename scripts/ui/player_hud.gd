13cb1137b307a38a7380ed58315f2949c38847fe
Rafail <rafail.borowski321@gmail.com>
2025-05-07 16:30:54 +0200

    feat: player hud added player hud and adjusted entity and enemy hud
    closes #40

--- scripts/ui/player_hud.gd
+++ scripts/ui/player_hud.gd
@@ -1,2 +1,16 @@
 class_name PlayerHud
 extends EntityHud
+
+var current_energy: int = 2
+var max_energy: int = 4
+var max_energy_deficit = -1
+
+func set_current_energy(value: int) -> void:
+	current_energy = value
+	
+func set_max_energy(value: int) -> void:
+	max_energy = value
+	
+func set_max_energy_deficit(value: int) -> void:
+	max_energy_deficit = value
+	
