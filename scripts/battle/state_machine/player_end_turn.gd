4c40fcd7de79bc742f5de24efb4e9107d254c974
Das_M_e_e_ <gabriel.eissler@gmail.com>
2025-05-14 19:10:34 +0200

    feat: discard hand at player turn end
    Added a function to discard the players hand at the end of his turn. Additionally implemented the drawing and discarding into the state machine.
    
    closes #86

--- scripts/battle/state_machine/player_end_turn.gd
+++ scripts/battle/state_machine/player_end_turn.gd
@@ -6,7 +6,10 @@ func enter():
 	# 1. resolve player end of turn effects
 	# 2. discard hand
 	# 3. enter state enemy_start_turn
-	await get_tree().create_timer(2.0).timeout
+	
+	EventBusHandler.call_event(EventBus.Event.PLAYER_TURN_END)
+	
+	await get_tree().create_timer(1.0).timeout
 	state_machine.transition_to("EnemyStartTurn")
 
 func exit():
--- scripts/battle/state_machine/player_start_turn.gd
+++ scripts/battle/state_machine/player_start_turn.gd
@@ -4,11 +4,16 @@ extends State
 func enter():
 	print("Entered PlayerStartTurn")
 	battle_ui.set_end_turn_button_enabled(false)
-	await get_tree().create_timer(2.0).timeout
+	
 	# 1. choose enemy intent
 	# 2. resolve start of turn effects
 	# 3. draw cards
 	# 4. enter state idle
+	
+	EventBusHandler.call_event(EventBus.Event.PLAYER_TURN_START)
+	
+	await get_tree().create_timer(1.0).timeout
+	
 	state_machine.transition_to("Idle")
 
 func exit():
