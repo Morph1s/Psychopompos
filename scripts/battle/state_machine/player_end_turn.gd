3a5123bbc41a62243ed2d15bcc065091cf69f5a5
Das_M_e_e_ <gabriel.eissler@gmail.com>
2025-05-21 16:08:16 +0200

    fix: fix timer await in player_end_turn

--- scripts/battle/state_machine/player_end_turn.gd
+++ scripts/battle/state_machine/player_end_turn.gd
@@ -9,7 +9,7 @@ func enter():
 	# 1. resolve player end of turn effects
 	EventBusHandler.call_event(EventBus.Event.PLAYER_TURN_END_EFFECTS)
 	# 2. discard hand
-	await get_tree().create_timer(0.5)
+	await get_tree().create_timer(0.5).timeout
 	discard_hand.emit()
 	# 3. enter state enemy_start_turn
 	EventBusHandler.call_event(EventBus.Event.PLAYER_TURN_END)
