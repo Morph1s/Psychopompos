31bf57e6c641e4c12a6765ff90757babe90c9f6e
Das_M_e_e_ <gabriel.eissler@gmail.com>
2025-06-12 20:11:32 +0200

    feat: map progress functionality
    you can no longer access every single encounter on the map, you start at the first layer and from there you can only access the encounters in the next layer that are connected to your current encounter with a line.

--- scripts/map/map.gd
+++ scripts/map/map.gd
@@ -15,13 +15,14 @@ var encounter_icons: Array[Texture] = [
 	preload("res://assets/graphics/map/icon_random.png"),
 ]
 var encounter_to_button: Dictionary = {}
+var current_encounter: Encounter
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Connect signals to buttons
 	exit_button.pressed.connect(_on_exit_button_pressed)
 
 func _process(delta: float) -> void:
+	# update the connection lines on the map
 	connection_drawer.queue_redraw()
 
 
@@ -29,6 +30,7 @@ func load_layers(map_layers):
 	for child in hbox.get_children():
 		child.queue_free()
 	
+	# each layer gets a VBoxContainer for displaying its encounters
 	for layer: MapLayer in map_layers:
 		var layer_container = VBoxContainer.new()
 		layer_container.mouse_filter = Control.MOUSE_FILTER_IGNORE
@@ -37,18 +39,46 @@ func load_layers(map_layers):
 		
 		for encounter: Encounter in layer.encounters:
 			var button = Button.new()
+			button.disabled = true
 			button.icon = encounter_icons[encounter.type]
 			button.pressed.connect(func(): _on_encounter_pressed(encounter))
 			layer_container.add_child(button)
 			encounter_to_button[encounter] = button
 	
+	# fill the encounter_to_button Dictionary
 	connection_drawer.set_connections(encounter_to_button)
+	
+	update_layer_states()
+
+func update_layer_states():
+	# at the start all the encounters in the start layer should be enabled
+	if current_encounter == null:
+		for button in hbox.get_child(0).get_children():
+			button.disabled = false
+		return
+	
+	# update the state of every encounter button on the map
+	for layer_container in hbox.get_children():
+		for button in layer_container.get_children():
+			var encounter = encounter_to_button.find_key(button)
+			button.disabled = not current_encounter.connections_to.has(encounter)
+			# change the appearance of already visited or the current encounter
+			if encounter == current_encounter:
+				button.modulate = Color(0.3, 0.3, 0.4)
+				button.disabled = true
+			elif encounter.completed:
+				button.modulate = Color(0.5, 0.5, 0.5)
+				button.disabled = true
+			else:
+				button.modulate = Color(1, 1, 1)
 
 func _on_encounter_pressed(encounter: Encounter):
 	hide()
 	encounter_selected.emit(encounter)
+	current_encounter = encounter
+	encounter.completed = true
+	update_layer_states()
 
-# Called when the exit button gets pressed
 func _on_exit_button_pressed():
 	print("exit button pressed")
 	hide()
--- scripts/map/map_connection_drawer.gd
+++ scripts/map/map_connection_drawer.gd
@@ -3,18 +3,20 @@ extends Control
 
 var connections: Array[Dictionary] = []
 
+# creates a Dictionary for each connection line that contains the buttons that should be connected
 func set_connections(encounter_to_button: Dictionary):
 	connections.clear()
 	
 	for encounter: Encounter in encounter_to_button.keys():
-		var button_from = encounter_to_button[encounter]
+		var button_from = encounter_to_button[encounter]        # pos 1
 		for target in encounter.connections_to:
 			if target in encounter_to_button:
-				var button_to = encounter_to_button[target]
+				var button_to = encounter_to_button[target]     # pos 2
 				connections.append({
 					"from": button_from,
 					"to": button_to,
 				})
+	# initial drawing of the connection lines
 	queue_redraw()
 
 func _draw():
@@ -22,9 +24,11 @@ func _draw():
 		var button_from = conn["from"]
 		var button_to = conn["to"]
 		
-		if not button_from.visible or not button_to.visible:
-			continue
+		# don't draw the line when both associated buttons are not visible
+		if not button_from.visible and not button_to.visible:
+			return
 		
+		# calculate the starting and end points of the line
 		var pos_from = button_from.get_global_position() + (button_from.size * 0.5) - self.get_global_position()
 		var pos_to = button_to.get_global_position() + (button_to.size * 0.5) - self.get_global_position()
 		
