91d799ad103cf03e82a76a309b5e41855fe44fab
Das_M_e_e_ <gabriel.eissler@gmail.com>
2025-06-12 19:24:03 +0200

    feat: simple map generation algorithm
    implemented a simple algorithm that welds nodes/encounters in a layer together, uniting their connections from and to nodes/encounters in other layers.

--- scripts/map/map.gd
+++ scripts/map/map.gd
@@ -16,6 +16,15 @@ var encounter_icons: Array[Texture] = [
 ]
 var encounter_to_button: Dictionary = {}
 
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Connect signals to buttons
+	exit_button.pressed.connect(_on_exit_button_pressed)
+
+func _process(delta: float) -> void:
+	connection_drawer.queue_redraw()
+
+
 func load_layers(map_layers):
 	for child in hbox.get_children():
 		child.queue_free()
@@ -23,6 +32,7 @@ func load_layers(map_layers):
 	for layer: MapLayer in map_layers:
 		var layer_container = VBoxContainer.new()
 		layer_container.mouse_filter = Control.MOUSE_FILTER_IGNORE
+		layer_container.size_flags_vertical = Control.SIZE_SHRINK_CENTER
 		hbox.add_child(layer_container)
 		
 		for encounter: Encounter in layer.encounters:
@@ -38,27 +48,6 @@ func _on_encounter_pressed(encounter: Encounter):
 	hide()
 	encounter_selected.emit(encounter)
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	# Connect signals to buttons
-	exit_button.pressed.connect(_on_exit_button_pressed)
-
-func _process(delta: float) -> void:
-	connection_drawer.queue_redraw()
-
-## Called when the battle button gets pressed
-#func _on_battle_button_pressed():
-	#print("battle button pressed")
-	#var encounter_data = "battle"
-	#hide()
-	#encounter_selected.emit(encounter_data)
-	#
-## Placeholder, called when another than the battle button gets pressed
-#func _on_non_battle_button_pressed():
-	#print("non battle button pressed")
-	#hide()
-	#close_map.emit()
-#
 # Called when the exit button gets pressed
 func _on_exit_button_pressed():
 	print("exit button pressed")
--- scripts/map/map_generator.gd
+++ scripts/map/map_generator.gd
@@ -1,32 +1,24 @@
 class_name MapGenerator
 extends Node
 
+const MAX_NUM_ENCOUNTERS_PER_LAYER = 5
+const MIN_NUM_ENCOUNTERS_PER_LAYER = 2
+const NUM_WELD_ITERATIONS = 25
+
 func generate_map() -> Array[MapLayer]:
 	var map_layers: Array[MapLayer] = []
 	
 	# start layer (contains only battles)
 	var start_layer = MapLayer.new()
 	start_layer.type = MapLayer.MapLayerType.START
-	
-	var start_layer_num_encounters = randi_range(2, 5)
-	for i in range(start_layer_num_encounters):
-		var encounter = Encounter.new()
-		encounter.type = Encounter.EncounterType.BATTLE
-		start_layer.encounters.append(encounter)
-	
+	_initial_fill_layer(start_layer)
 	map_layers.append(start_layer)
 	
 	# first half of normal layers
-	for layer in range(5):
+	for layer in range(4):
 		var map_layer = MapLayer.new()
 		map_layer.type = MapLayer.MapLayerType.NORMAL
-		
-		var num_encounters = randi_range(2, 5)
-		for i in range(num_encounters):
-			var encounter = Encounter.new()
-			encounter.type = Encounter.EncounterType.BATTLE
-			map_layer.encounters.append(encounter)
-		
+		_initial_fill_layer(map_layer)
 		map_layers.append(map_layer)
 	
 	# mini boss layer
@@ -34,7 +26,7 @@ func generate_map() -> Array[MapLayer]:
 	mini_boss_layer.type = MapLayer.MapLayerType.MINI_BOSS
 	
 	var mini_boss_encounter = Encounter.new()
-	mini_boss_encounter.type = Encounter.EncounterType.BATTLE
+	#mini_boss_encounter.type = Encounter.EncounterType.BATTLE
 	mini_boss_layer.encounters.append(mini_boss_encounter)
 	
 	map_layers.append(mini_boss_layer)
@@ -43,13 +35,7 @@ func generate_map() -> Array[MapLayer]:
 	for layer in range(5):
 		var map_layer = MapLayer.new()
 		map_layer.type = MapLayer.MapLayerType.NORMAL
-		
-		var num_encounters = randi_range(2, 5)
-		for i in range(num_encounters):
-			var encounter = Encounter.new()
-			encounter.type = Encounter.EncounterType.BATTLE
-			map_layer.encounters.append(encounter)
-		
+		_initial_fill_layer(map_layer)
 		map_layers.append(map_layer)
 	
 	# boss layer
@@ -57,36 +43,66 @@ func generate_map() -> Array[MapLayer]:
 	boss_layer.type = MapLayer.MapLayerType.BOSS
 	
 	var boss_encounter = Encounter.new()
-	boss_encounter.type = Encounter.EncounterType.BATTLE
+	#boss_encounter.type = Encounter.EncounterType.BATTLE
 	boss_layer.encounters.append(boss_encounter)
 	
 	map_layers.append(boss_layer)
 	
-	# set connections
-	for layer in range(len(map_layers) - 1):
-		# if next layer is the mini boss, set all connections to the mini boss encounter
-		if map_layers[layer + 1].type == MapLayer.MapLayerType.MINI_BOSS:
-			for encounter: Encounter in map_layers[layer].encounters:
-				encounter.connections_to = map_layers[layer + 1].encounters
-			continue
-		
-		# if next layer is the boss, set all connections to the boss encounter
-		elif map_layers[layer + 1].type == MapLayer.MapLayerType.BOSS:
-			for encounter: Encounter in map_layers[layer].encounters:
-				encounter.connections_to = map_layers[layer + 1].encounters
-			continue
-		
-		# if this layer ist the mini boss, set connections to all encounters of the next layer
-		elif map_layers[layer].type == MapLayer.MapLayerType.MINI_BOSS:
-			for encounter: Encounter in map_layers[layer].encounters:
-				encounter.connections_to = map_layers[layer + 1].encounters
-		
-		# if next layer is a normal layer, set random connections
-		else:
-			for encounter: Encounter in map_layers[layer].encounters:
-				var next_encounters = map_layers[layer + 1].encounters.duplicate()
-				next_encounters.shuffle()
-				var num_next_encounters = randi_range(1, 3)
-				encounter.connections_to = next_encounters.slice(0, min(num_next_encounters, next_encounters.size()))
+	# set initial connections
+	_set_initial_connections(map_layers)
+	
+	for i in NUM_WELD_ITERATIONS:
+		_weld_iteration(map_layers)
 	
 	return map_layers
+
+func _initial_fill_layer(layer: MapLayer):
+	for i in range(MAX_NUM_ENCOUNTERS_PER_LAYER):
+		var encounter: Encounter = Encounter.new()
+		layer.encounters.append(encounter)
+
+func _set_initial_connections(map_layers: Array[MapLayer]):
+	for layer: MapLayer in map_layers:
+		# the last layer (boss layer) should not have any outgoing connections
+		if layer.type == MapLayer.MapLayerType.BOSS:
+			break
+		
+		# we need the following layer to connect encounters
+		var next_layer = map_layers[map_layers.find(layer) + 1]
+		
+		# if the following layer is the mini boss or boss layer, connect every encounter to the mini boss or boss encounter
+		# if this layer is the mini boss layer, also connect it to all encounters of the following layer
+		if ((next_layer.type == MapLayer.MapLayerType.MINI_BOSS) 
+			or (next_layer.type == MapLayer.MapLayerType.BOSS)
+			or (layer.type == MapLayer.MapLayerType.MINI_BOSS)):
+			for encounter: Encounter in layer.encounters:
+				encounter.connections_to = next_layer.encounters
+			continue
+		
+		# in all other cases, connect the first encounter of this layer to the first of the next, the second of this to the second of the next, etc.
+		for encounter: Encounter in layer.encounters:
+			encounter.connections_to.append(next_layer.encounters[layer.encounters.find(encounter)])
+
+func _weld_iteration(map_layers: Array[MapLayer]):
+	# choose a random layer
+	var layer = map_layers[randi_range(0, map_layers.size() - 1)]
+	
+	# the chosen layer needs at least 2 encounter nodes
+	# mini boss and boss layers have only one, so we dont need to check them separately
+	if layer.encounters.size() > MIN_NUM_ENCOUNTERS_PER_LAYER:
+		# choose a random encounter in the layer, that has at least one encounter with a higher index
+		var node_1: Encounter = layer.encounters[randi_range(0, layer.encounters.size() - 2)]
+		var node_2: Encounter = layer.encounters[layer.encounters.find(node_1) + 1]
+		# transfer the connections from node_2 to node_1
+		for encounter: Encounter in node_2.connections_to:
+			if not node_1.connections_to.has(encounter):
+				node_1.connections_to.append(encounter)
+		# transfer the connections to node_2 to node_1
+		if not layer.type == MapLayer.MapLayerType.START:
+			var previous_layer = map_layers[map_layers.find(layer) - 1]
+			for encounter: Encounter in previous_layer.encounters:
+				if encounter.connections_to.has(node_2) and not encounter.connections_to.has(node_1):
+					encounter.connections_to.erase(node_2)
+					encounter.connections_to.append(node_1)
+		# delete node_2 from the layer
+		layer.encounters.erase(node_2)
