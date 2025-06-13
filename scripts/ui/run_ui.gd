9d5395f8935919fcd42d24e8e60ee1956b4f2807
Morph1s <lottjedesmond@gmail.com>
2025-06-13 21:28:39 +0200

    feat: tooltips closes #132

--- scripts/ui/run_ui.gd
+++ scripts/ui/run_ui.gd
@@ -3,6 +3,12 @@ extends TextureRect
 
 signal open_map
 
+@onready var tooltip = $Tooltip
+
+func _ready() -> void:
+	EventBusHandler.show_tooltips.connect(_on_eventbus_show_tooltips)
+	EventBusHandler.hide_tooltips.connect(_on_eventbus_hide_tooltips)
+
 func _on_deck_icon_gui_input(event: InputEvent) -> void:
 	if event.is_action_released("left_click"):
 		pass
@@ -14,3 +20,10 @@ func _on_map_icon_gui_input(event: InputEvent) -> void:
 func _on_settings_icon_gui_input(event: InputEvent) -> void:
 	if event.is_action_released("left_click"):
 		EventBusHandler.open_settings.emit()
+
+func _on_eventbus_show_tooltips(data: Array[TooltipData]) -> void:
+	tooltip.load_tooltips(data)
+	tooltip.show()
+
+func _on_eventbus_hide_tooltips() -> void:
+	tooltip.hide()
---
+++ scripts/ui/tooltip.gd
@@ -0,0 +1,26 @@
+class_name Tooltip
+extends Control
+
+@onready var descriptions_container = $Background/DescriptionsContainer
+
+const MAX_LABEL_WIDTH: int = 100
+
+func load_tooltips(data: Array[TooltipData]) -> void:
+	for child in descriptions_container.get_children():
+		child.queue_free()
+	
+	for entry in data:
+		var entry_container = HBoxContainer.new()
+		descriptions_container.add_child(entry_container)
+		
+		if entry.icon:
+			var icon = TextureRect.new()
+			icon.stretch_mode = TextureRect.STRETCH_KEEP
+			icon.texture = entry.icon
+			entry_container.add_child(icon)
+		
+		var label = Label.new()
+		label.text = entry.description
+		label.autowrap_mode = TextServer.AUTOWRAP_WORD
+		label.custom_minimum_size = theme.default_font.get_multiline_string_size(entry.description, HORIZONTAL_ALIGNMENT_LEFT, MAX_LABEL_WIDTH, 6)
+		entry_container.add_child(label)
