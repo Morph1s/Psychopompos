d2431efb514e275b7e8ba23590583ab836982306
Morph1s <lottjedesmond@gmail.com>
2025-06-25 21:23:01 +0200

    feat: applied sprites
    added sprites to card background, tooltips, card effect icons, main menu, hud, effects and enemy intents. Implemented entity hud hp bar. relocated end turn button and energy balls.

--- scripts/ui/entity_hud.gd
+++ scripts/ui/entity_hud.gd
@@ -1,28 +1,47 @@
 extends Control
 class_name EntityHud
 
+const PLAYER_HP_BAR_OVER = preload("res://assets/graphics/hud/hp_bar/player_hp_bar_over.png")
+const ENTITY_DEFEND_BAR_OVER = preload("res://assets/graphics/hud/hp_bar/entity_defend_bar_over.png")
+
+@onready var hp_bar = $HpBar
+@onready var hitpoints = $HpBar/LabelContainer/Hitpoints
+@onready var block = $HpBar/LabelContainer/Block
+
 var current_hp: int = 0
 var max_hp: int = 100
 var block_value: int = 0
 
-@onready var hitpoints = $HitpointBackground/HitpointContainer/Hitpoints
-@onready var block = $HitpointBackground/HitpointContainer/Block
 
 func set_current_hp(value: int) -> void:
 	current_hp = value
+	hp_bar.value = value
 	_update_display()
 	
 func set_max_hp(value: int) -> void:
-	max_hp = value	
+	max_hp = value
+	hp_bar.max_value = value
 	_update_display()
 
 func set_block(value: int) -> void:
 	block_value = value
+	_set_hp_bar_boarder()
 	_update_display()
 
 func _update_display() -> void:
-	hitpoints.text = "%d/%d" % [current_hp, max_hp]
-	block.text = "+%d" % [block_value]
+	hitpoints.text = str(current_hp)
+	
+	if block_value:
+		block.text = "+%d" % [block_value]
+		block.show()
+	else:
+		block.hide()
 
 func set_entity_size(size: Vector2) -> void:
 	custom_minimum_size = size + Vector2(0, 8)
+
+func _set_hp_bar_boarder() -> void:
+	if block_value:
+		hp_bar.texture_over = ENTITY_DEFEND_BAR_OVER
+	else:
+		hp_bar.texture_over = PLAYER_HP_BAR_OVER
