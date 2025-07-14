58f142ee43702644bfc268922818dfd72ac15b93
Morph1s <lottjedesmond@gmail.com>
2025-07-14 13:12:13 +0200

    feat: refined tooltips
    added logic for plural for words with an 's' suffix, and logic for hiding the value if its 1

--- scripts/custom_resources/tooltip_data.gd
+++ scripts/custom_resources/tooltip_data.gd
@@ -5,18 +5,31 @@ extends Resource
 @export_multiline var description_pre_value: String
 @export var use_value: bool = false
 @export var default_value: int = 1
+@export var dynamic_plural: bool = false
+@export var hide_value_when_singular: bool = false
+@export var singular: String
 @export_multiline var description_after_value: String
 
 var description: String = ""
 
 func set_description(value: int = -1) -> void:
+	var we_have_string_builders_at_home: String = description_pre_value
+	
 	if use_value:
 		if value == -1:
-			description = description_pre_value + " " + str(default_value) + " " + description_after_value
+			we_have_string_builders_at_home = we_have_string_builders_at_home + " " + str(default_value)
 		else:
-			description = description_pre_value + " " + str(value) + " " + description_after_value
+			we_have_string_builders_at_home = we_have_string_builders_at_home + " " + str(value)
 	
-	elif description_after_value.is_empty():
-		description = description_pre_value
-	else:
-		description = description_pre_value + " " + description_after_value
+	if dynamic_plural:
+		if value > 1:
+			we_have_string_builders_at_home = we_have_string_builders_at_home + " " + singular + "s"
+		elif not hide_value_when_singular:
+			we_have_string_builders_at_home = we_have_string_builders_at_home + " " + singular
+		else: 
+			we_have_string_builders_at_home = description_pre_value + " " + singular
+	
+	if not description_after_value.is_empty():
+		we_have_string_builders_at_home = we_have_string_builders_at_home + " " + description_after_value
+	
+	description = we_have_string_builders_at_home
