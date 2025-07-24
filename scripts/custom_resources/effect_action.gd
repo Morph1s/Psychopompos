cbaf3048e1184c7580322f551a2a55953ebe4ce1
Das_M_e_e_ <gabriel.eissler@gmail.com>
2025-07-24 16:59:25 +0200

    feat: Card Caeneus
    added Caeneus resource, added new effect "Caeneus"

--- scripts/custom_resources/effect_action.gd
+++ scripts/custom_resources/effect_action.gd
@@ -12,6 +12,7 @@ enum EffectType {
 	WARRIORS_FURRY,
 	WOUNDED,
 	BLESSING,
+	CAENEUS,
 }
  
 var effect_names: Dictionary = {
@@ -25,6 +26,7 @@ var effect_names: Dictionary = {
 	EffectType.WARRIORS_FURRY: "WarriorsFury",
 	EffectType.WOUNDED: "Wounded",
 	EffectType.BLESSING: "Blessing",
+	EffectType.CAENEUS: "Caeneus",
 }
 
 @export var effect: EffectType
